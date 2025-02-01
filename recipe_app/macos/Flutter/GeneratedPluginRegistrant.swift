02149c16c9bd5f294462a32078b0808ef80ef05d
unknown <meroo1995saggaf@outlook.com>
2025-02-01 13:39:16 +0300

    finish the code

--- recipe_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ recipe_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,14 +9,12 @@ import firebase_core
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
-import video_player_avfoundation
-import wakelock_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
